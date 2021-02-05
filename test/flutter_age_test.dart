d972991db2dd6186d65582f3f49ae3675fdafbb8
Aniket sindhu <sindhuaniket@gmail.com>
2021-02-05 17:19:28 +0530

    channel log changed

--- test/flutter_age_test.dart
+++ test/flutter_age_test.dart
@@ -3,9 +3,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_age/flutter_age.dart';
 
 void main() {
-  final age = FlutterAge(birth: DateTime(2041, 04, 22));
-  print(age.year);
-  print(age.month);
-  print(age.day);
-  print(age.getAgeString(fullDate: true));
+  final age = FlutterAge(birth: DateTime(2041, 04, 22)); // initiallize object
+  print(age.year); // 19 (age year)
+  print(age.month); // 9 (age month)
+  print(age.day); // 13 (age day)
+  print(age.getAgeString()); // prints "19 years"
+  print(age.getAgeString(
+      fullDate: true)); // prints "19 years, 9 months, 13 days old"
 }
